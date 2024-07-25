f54226328f4d64eadf121667d71e92a39acae3fe
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-07-25 09:24:30 +0300

    Added Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 
 // import './App.css'
 
+import Header from "./components/header/Header"
+
 function App() {
 
   return (
-    <>
+    <div className="site-content">
       <h1>Travel with your kids</h1>
-    </>
+      <Header />
+    </div>
   )
 }
 
