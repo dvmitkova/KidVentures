a297cbf66d2358ee29743e4373225507f159ccb3
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-07-25 09:36:18 +0300

    Revert "Added Header component"
    This reverts commit f54226328f4d64eadf121667d71e92a39acae3fe.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 
 // import './App.css'
 
-import Header from "./components/header/Header"
-
 function App() {
 
   return (
-    <div className="site-content">
+    <>
       <h1>Travel with your kids</h1>
-      <Header />
-    </div>
+    </>
   )
 }
 
