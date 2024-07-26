f9fc3b426baca1737f07dad371b807844eb5585e
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-07-26 09:56:26 +0300

    Added comments section

--- client/src/api/commentsAPI.js
+++ client/src/api/commentsAPI.js
@@ -1,16 +1,15 @@
 import requester from "./requester"
 
-const BASE_URL = 'http://localhost:3030/jsonstore/trips';
+const BASE_URL = 'http://localhost:3030/jsonstore/trips/trips/details';
 const buildUrl = (tripId) => `${BASE_URL}/${tripId}/comments`
 
 const create = async (tripId, username, text) => requester.post(buildUrl(tripId), { username, text });
-
 
 const getAll = async (tripId) => {
     const result = await requester.get(buildUrl(tripId))
     const comments = Object.values(result);
 
-    return comments;
+    console.log(comments);
 }
 
 export default {
--- client/src/components/trip-details/TripDetails.jsx
+++ client/src/components/trip-details/TripDetails.jsx
@@ -28,6 +28,8 @@ export default function TripDetails() {
   const commentSubmitHandler = async (e) => {
     e.preventDefault();
 
+    console.log(username, comment);
+
     const newComment = await commentsAPI.create(tripId, username, comment);
 
     //TODO: this should be refactored
@@ -48,8 +50,8 @@ export default function TripDetails() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-    <Card sx={{ width: 600, height: 'auto', margin: 4 }}>
+    <div className="flex flex-row justify-center items-start min-h-screen">
+    <Card sx={{ width: '100%', height: 'auto', margin: 4 }}>
         <CardActionArea>
             <CardMedia
                 component="img"
@@ -82,7 +84,65 @@ export default function TripDetails() {
                 </div>
             </CardContent>
         </CardActionArea>
-    </Card>
+      </Card>
+      
+      {/* Comments Section */}
+      <div className="flex flex-col items-center mr-10 w-full">
+        <Card sx={{ width: '100%', maxWidth: 600, height: 'auto', margin: 4 }}>
+          <CardContent>
+          <div className="items-center text-sm font-medium">
+              <h1 className="text-xl font-bold text-cyan-950 mb-8">Share your thoughts with us:</h1>
+            </div>
+            <form onSubmit={commentSubmitHandler} className="space-y-4">
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700">Your Name</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="mt-2 block w-full rounded-md border-gray-300 shadow focus:border-orange-200 focus:ring focus:ring-orange-200 focus:ring-opacity-100"
+                />
+              </div>
+              <div>
+                <label htmlFor="comment" className="block text-sm font-medium text-gray-700">Comment</label>
+                <textarea
+                  id="comment"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  className="mt-2 block w-full rounded-md border-gray-300 shadow focus:border-orange-200 focus:ring focus:ring-orange-200 focus:ring-opacity-100"
+                />
+              </div>
+              <button type="submit" className="w-full p-2 bg-stone-200 text-cyan-950 rounded hover:bg-orange-200 hover:shadow">
+                Add comment
+              </button>
+            </form>
+            <div className="mt-4 space-y-4">
+
+
+              
+              {Object.keys(trip.comments || {}).length > 0
+                ? Object.values(trip.comments).map((comment) => (
+                  <>
+                  <div className="items-center text-sm font-medium">
+                  <h1 className="text-xl font-bold text-cyan-950 mt-8 mb-6">All comments:</h1>
+                </div>
+                <div key={comment._id} className="p-4 bg-white shadow rounded-md">
+                  <Typography variant="body2" color="#083344" sx={{ fontWeight: 'bold' }}>
+                    {comment.username}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {comment.text}
+                  </Typography>
+                    </div>
+                    </>
+              ))
+                : <h3 className="text-cyan-950 mt-6">No comments yet.</h3>
+              }
+            </div>
+          </CardContent>
+        </Card>
+      </div>
 </div>
   );
 }
