bc43a40980c5757e87097cb72ddf80d5a52d6616
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-08-05 14:08:46 +0300

    Fixed Latest Trips

--- client/src/components/trips-all/TripsAll.jsx
+++ client/src/components/trips-all/TripsAll.jsx
@@ -3,23 +3,34 @@ import { Box } from "@mui/material";
 import { useTripsGetAll } from "../../hooks/useTrips";
 
 export default function TripsAll() {
-        const [trips] = useTripsGetAll()
-    
-        return (
-            <Box
-            id="catalog-page"
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                gap: '16px',
-                justifyContent: 'center',
-                padding: '20px'
-            }}
-        >
-            {trips.length > 0
-                ? trips.map(trip => <TripsListItem key={trip._id} {...trip} />)
-                : <h3 style={{ textAlign: 'center', width: '100%', marginTop: '20px' }}>No trips yet</h3>
-            }
-        </Box>
-        )
+  const [trips] = useTripsGetAll();
+
+  return (
+    <section>
+      <div className="top-20 left-0 right-0 mx-auto max-w-2xl text-center">
+        <h1 className="text-3xl p-10 font-bold text-cyan-800 sm:text-3xl">
+          All Trips
+        </h1>
+      </div>
+
+      <Box
+        id="catalog-page"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "16px",
+          justifyContent: "center",
+          padding: "20px",
+        }}
+      >
+        {trips.length > 0 ? (
+          trips.map((trip) => <TripsListItem key={trip._id} {...trip} />)
+        ) : (
+          <h3 style={{ textAlign: "center", width: "100%", marginTop: "20px" }}>
+            No trips yet
+          </h3>
+        )}
+      </Box>
+    </section>
+  );
 }
--- client/src/components/trips-latest/TripsLatest.jsx
+++ client/src/components/trips-latest/TripsLatest.jsx
@@ -4,31 +4,41 @@ import { useEffect, useState } from "react";
 import tripsAPI from "../../api/tripsAPI";
 
 export default function TripsLatest() {
-    const [latestTrips, setLatestTrips] = useState([]);
+  const [latestTrips, setLatestTrips] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            const result = await tripsAPI.getLatest();
+  useEffect(() => {
+    (async () => {
+        const result = await tripsAPI.getAll();
 
-            setLatestTrips(result);
-        })();
-    }, [])
-    
-        return (
-            <Box
-            id="catalog-page"
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                gap: '16px',
-                justifyContent: 'center',
-                padding: '20px'
-            }}
-        >
-            {latestTrips.length > 0
-                ? latestTrips.map(trip => <TripsListItem key={trip._id} {...trip} />)
-                : <h3 style={{ textAlign: 'center', width: '100%', marginTop: '20px' }}>No trips yet</h3>
-            }
-        </Box>
-        )
+        setLatestTrips(result.reverse().slice(0, 3))
+    })();
+}, [])
+
+  return (
+    <section>
+      <div className="top-20 left-0 right-0 mx-auto max-w-2xl text-center">
+        <h1 className="text-3xl p-10 font-bold text-cyan-800 sm:text-3xl">
+          Latest Trips
+        </h1>
+      </div>
+      <Box
+        id="catalog-page"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "16px",
+          justifyContent: "center",
+          padding: "20px",
+        }}
+      >
+        {latestTrips.length > 0 ? (
+          latestTrips.map((trip) => <TripsListItem key={trip._id} {...trip} />)
+        ) : (
+          <h3 style={{ textAlign: "center", width: "100%", marginTop: "20px" }}>
+            No trips yet
+          </h3>
+        )}
+      </Box>
+    </section>
+  );
 }
